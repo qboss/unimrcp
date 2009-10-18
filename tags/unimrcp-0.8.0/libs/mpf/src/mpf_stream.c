978df4bbc9df7fc87e978f9c50e413c80c3283dc
achaloyan <achaloyan@f001bc3a-424a-0410-80a0-a715b8f413a8>
2009-10-18 17:57:00 +0000

    Added release tag unimrcp-0.8.0

---
+++ tags/unimrcp-0.8.0/libs/mpf/src/mpf_stream.c
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2008 Arsen Chaloyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mpf_stream.h"
+
+/** Create stream capabilities */
+MPF_DECLARE(mpf_stream_capabilities_t*) mpf_stream_capabilities_create(mpf_stream_direction_e direction, apr_pool_t *pool)
+{
+	mpf_stream_capabilities_t *capabilities = (mpf_stream_capabilities_t*)apr_palloc(pool,sizeof(mpf_stream_capabilities_t));
+	capabilities->direction = direction;
+	mpf_codec_capabilities_init(&capabilities->codecs,1,pool);
+	return capabilities;
+}
+
+/** Clone stream capabilities */
+MPF_DECLARE(mpf_stream_capabilities_t*) mpf_stream_capabilities_clone(const mpf_stream_capabilities_t *src_capabilities, apr_pool_t *pool)
+{
+	mpf_stream_capabilities_t *capabilities = (mpf_stream_capabilities_t*)apr_palloc(pool,sizeof(mpf_stream_capabilities_t));
+	capabilities->direction = src_capabilities->direction;
+	mpf_codec_capabilities_clone(&capabilities->codecs,&src_capabilities->codecs,pool);
+	return capabilities;
+}
+
+/** Merge stream capabilities */
+MPF_DECLARE(apt_bool_t) mpf_stream_capabilities_merge(mpf_stream_capabilities_t *capabilities, const mpf_stream_capabilities_t *src_capabilities, apr_pool_t *pool)
+{
+	capabilities->direction |= src_capabilities->direction;
+	return mpf_codec_capabilities_merge(&capabilities->codecs,&src_capabilities->codecs,pool);
+}
+
+
+
+/** Create audio stream */
+MPF_DECLARE(mpf_audio_stream_t*) mpf_audio_stream_create(void *obj, const mpf_audio_stream_vtable_t *vtable, const mpf_stream_capabilities_t *capabilities, apr_pool_t *pool)
+{
+	mpf_audio_stream_t *stream = (mpf_audio_stream_t*)apr_palloc(pool,sizeof(mpf_audio_stream_t));
+	stream->obj = obj;
+	stream->vtable = vtable;
+	stream->termination = NULL;
+	stream->capabilities = capabilities;
+	stream->direction = capabilities ? capabilities->direction : STREAM_DIRECTION_NONE;
+	stream->rx_descriptor = NULL;
+	stream->rx_event_descriptor = NULL;
+	stream->tx_descriptor = NULL;
+	stream->tx_event_descriptor = NULL;
+	return stream;
+}
+
+/** Validate audio stream receiver */
+MPF_DECLARE(apt_bool_t) mpf_audio_stream_rx_validate(mpf_audio_stream_t *stream, const mpf_codec_descriptor_t *descriptor, apr_pool_t *pool)
+{
+	if(!stream->capabilities) {
+		return FALSE;
+	}
+
+	if(stream->rx_descriptor) {
+		return TRUE;
+	}
+
+	stream->rx_descriptor = mpf_codec_descriptor_create_by_capabilities(&stream->capabilities->codecs,descriptor,pool);
+	return stream->rx_descriptor ? TRUE : FALSE;
+}
+
+/** Validate audio stream transmitter */
+MPF_DECLARE(apt_bool_t) mpf_audio_stream_tx_validate(mpf_audio_stream_t *stream, const mpf_codec_descriptor_t *descriptor, apr_pool_t *pool)
+{
+	if(!stream->capabilities) {
+		return FALSE;
+	}
+
+	if(stream->tx_descriptor) {
+		return TRUE;
+	}
+
+	stream->tx_descriptor = mpf_codec_descriptor_create_by_capabilities(&stream->capabilities->codecs,descriptor,pool);
+	return stream->tx_descriptor ? TRUE : FALSE;
+}
---
+++ tags/unimrcp-0.8.0/libs/mpf/src/mpf_termination.c
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2008 Arsen Chaloyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mpf_termination.h"
+#include "mpf_stream.h"
+#include "mpf_codec_manager.h"
+
+MPF_DECLARE(mpf_termination_t*) mpf_termination_base_create(
+										mpf_termination_factory_t *termination_factory,
+										void *obj,
+										const mpf_termination_vtable_t *vtable,
+										mpf_audio_stream_t *audio_stream,
+										mpf_video_stream_t *video_stream,
+										apr_pool_t *pool)
+{
+	mpf_termination_t *termination = apr_palloc(pool,sizeof(mpf_termination_t));
+	termination->pool = pool;
+	termination->obj = obj;
+	termination->event_handler_obj = NULL;
+	termination->event_handler = NULL;
+	termination->codec_manager = NULL;
+	termination->termination_factory = termination_factory;
+	termination->vtable = vtable;
+	termination->slot = 0;
+	if(audio_stream) {
+		audio_stream->termination = termination;
+	}
+	if(video_stream) {
+		video_stream->termination = termination;
+	}
+	termination->audio_stream = audio_stream;
+	termination->video_stream = video_stream;
+	return termination;
+}
+
+MPF_DECLARE(apt_bool_t) mpf_termination_destroy(mpf_termination_t *termination)
+{
+	if(termination->vtable && termination->vtable->destroy) {
+		termination->vtable->destroy(termination);
+	}
+	return TRUE;
+}
+
+MPF_DECLARE(void*) mpf_termination_object_get(mpf_termination_t *termination)
+{
+	return termination->obj;
+}
+
+MPF_DECLARE(apt_bool_t) mpf_termination_modify(mpf_termination_t *termination, void *descriptor)
+{
+	if(termination->vtable && termination->vtable->modify) {
+		termination->vtable->modify(termination,descriptor);
+	}
+	return TRUE;
+}
+
+/** Create MPF termination from termination factory */
+MPF_DECLARE(mpf_termination_t*) mpf_termination_create(
+										mpf_termination_factory_t *termination_factory,
+										void *obj,
+										apr_pool_t *pool)
+{
+	if(termination_factory && termination_factory->create_termination) {
+		return termination_factory->create_termination(termination_factory,obj,pool);
+	}
+	return NULL;
+}
+
+/** Create raw MPF termination. */
+MPF_DECLARE(mpf_termination_t*) mpf_raw_termination_create(
+										void *obj,
+										mpf_audio_stream_t *audio_stream,
+										mpf_video_stream_t *video_stream,
+										apr_pool_t *pool)
+{
+	return mpf_termination_base_create(NULL,obj,NULL,audio_stream,video_stream,pool);
+}
